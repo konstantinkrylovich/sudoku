e0c22dcf6d54f29af4579176d73cf981da1eaeac
konstantinkrylovich <konstantinkrylovich@gmail.com>
2019-03-20 23:19:03 +0300

    pull from origin

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function solveSudoku(matrix) {
   // your solution
+  const findNextCellToFill = (matrix) => {
+    for(let i = 0; i < matrix.length; i++){
+      for(let j = 0; j < matrix[i].length; j++){
+        if (matrix[i][j] == 0) {
+          return [i, j];
+        }
+      }
+    }
+    return [-1, -1];
+  }
 }
